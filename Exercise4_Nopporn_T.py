5dfb2c7c1a9658d653bd310eb5a3ef5e1ae605f3
BigBoyLaboratory <64446495+BigBoyLaboratory@users.noreply.github.com>
2021-05-07 09:05:12 +0700

    Create Exercise4_Nopporn_T.py

---
+++ Exercise4_Nopporn_T.py
@@ -0,0 +1,9 @@
+FE  = 80
+GB  = 85
+ICS = 93
+CP  = 99
+print("---Your Score---")
+print("Foundation English : คะแนนที่ได้", FE)
+print("General Business : คะแนนที่ได้", GB)
+print("Introduction to Computer Systems : คะแนนที่ได้", ICS)
+print("Computer Programming : คะแนนที่ได้", CP)
